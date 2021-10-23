567b77ba2cd6bf865e17b96c0bb2bbba49b2468f
moisecas <62753698+moisecas@users.noreply.github.com>
2021-10-23 15:18:06 -0500

    Add files via upload

---
+++ Conversor.py
@@ -0,0 +1,6 @@
+pesos = input("cuantos pesos colombianos tiene?:")
+pesos = float(pesos) 
+valor_dolar = 3875
+dolares = pesos / valor_dolar 
+dolares = str(dolares) 
+print("tienes $" + dolares + "dolares") 
